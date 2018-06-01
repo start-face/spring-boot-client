e17d788694167f975e00c1a63f81884f5f54ee9b
tcaironglnan <995956062@qq.com>
2018-06-01 23:19:40 +0800

    1.fix

--- src/main/java/com/client/ServiceClientApplication.java
+++ src/main/java/com/client/ServiceClientApplication.java
@@ -1,11 +1,9 @@
 package com.client;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.client.dao")
 public class ServiceClientApplication {
 
     public static void main(String[] args) {
